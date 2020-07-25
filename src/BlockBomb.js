50a101801a8bf45111a18efd76369c7b97e9c7a7
Zach Nicholson <raiste.doctow@gmail.com>
2020-07-25 18:29:40 -0500

    text on top of bombs, added min height for site body

--- src/BlockBomb.js
+++ src/BlockBomb.js
@@ -1,23 +1,31 @@
 import React from 'react';
+import './BlockBomb.css';
 
 const BlockBomb = props => {
+
     const dragStart = e => {
         e.dataTransfer.setData("card_id", e.target.id);
-      };
+
+        // setTimeout(() => {
+        //     e.target.style.display = "none";
+        // }, 0);
+
+    };
     
     const dragOver = e => {
         e.stopPropagation();
     };
     
+
     return (
         <div className="block-bomb" id={`bomb-${props.block.id}`} draggable="true" onDragStart={dragStart} onDragOver={dragOver}>
             <img
                 src={props.bomb}
-                width="110"
-                height="80"
+                width="145"
+                height="110"
                 draggable="false"
             />
-            <p className="bomb-text">{props.block.description}</p>
+            <div className="bomb-text">{props.block.description}</div>
         </div>
     )
 }
