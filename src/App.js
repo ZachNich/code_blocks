0707a9d8e98026827211f0e3a11bda14c845f0dc
Zach Nicholson <raiste.doctow@gmail.com>
2020-07-25 12:39:03 -0500

    initial structure and some bootstrap added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import './App.css';
 
 function App() {
   return (
-      0
+    <div class="container">
+      <div class="row site-header">
+        <div class="col-sm-2">
+          First Row
+        </div>
+      </div>
+      <div class="row site-main justify-content-between">
+        <div class="col-sm-8 game-puzzle">
+          Second Row, First Column
+        </div>
+        <div class="col-sm game-pieces">
+          Second Row, Second Column
+        </div>
+      </div>
+    </div>
   );
 }
 
